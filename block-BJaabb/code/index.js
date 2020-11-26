a661eeae6369510ca47ae6bfc08a7f3a644cbc6e
ravindra singh <ravindrarajpoot9628172@gmail.com>
2020-11-26 19:21:48 +0530

    assignment 2

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -5,22 +5,57 @@
 3. Declare variable `age` and `isLearning` with value `22` and `true` in the same line. hint: declaring multiplemvariable in one line.
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
+let user = prompt("please enter your name");
+alert(`i am ${user}`);
+let userName;
+userName = user;
+console.log(userName);
+let age = 22 , isLearning=true;
+user = "John";
+console.log(user);
+console.log(userName);
 
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+let numA = Number(prompt("enter a number"));
+let numB = Number(prompt("enter a number"));
+let sum ;
+sum = numA + numB;
+console.log(sum  );
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10; j++){ console.log(j)};
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
+for(let i = 9 ; i>=1 ;i--){
+    if(i%2!==0){
+        console.log(i)
+    }
+}
 
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
+let num = 5;
+let a="";
+while(num>=0){
+    a+=String(num);
+    num--
+}
+console.log(a)
 
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
+let c = 10;
+let str="";
+while(c>=0){
+    if(c%2 === 0){
+        str+=c;
+    }
+    c--
+}
+console.log(str);
 
 // Comparisoin
 
@@ -28,11 +63,21 @@ for (let j = 0; j <= 10; ____) console.log(j);
 
 - `alert` true or false base on the entered value.
 - if the input value is anything like `true`, `null` or `undefined` alert saying `Enter a valid value`.
-
-
 Example:
 
 21, 21; // true
 "hello", -21; // false
 
 */
+let num1 = Number(prompt("please enter value num1"));
+let num2 = Number(prompt("please enter value num2"));
+if(num1===num2){
+    alert('equal');
+    alert('true')
+}else if(num1!==num2){
+    alert('not equal');
+    alert('false');
+}else if(num1===true && num2=== true){
+    alert('enter vailid number');
+}
+
